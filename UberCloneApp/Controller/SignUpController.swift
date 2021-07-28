e480783e2b1a941ff4ba981d495f9bb65b869d52
Magomed <Magomed.nagoev@yandex.ru>
2021-07-28 21:36:32 +0300

    created LoginController

---
+++ UberCloneApp/Controller/SignUpController.swift
@@ -0,0 +1,28 @@
+//
+//  SignUpController.swift
+//  UberCloneApp
+//
+//  Created by Нагоев Магомед on 28.07.2021.
+//
+
+import UIKit
+
+class SignUPController: UIViewController {
+    
+    
+    //MARK: -Properites
+    
+    //MARK: -Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .lightGray
+        
+    }
+    
+    
+    //MARK: -Selector
+    
+    
+}
