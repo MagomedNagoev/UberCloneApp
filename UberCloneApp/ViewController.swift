e9e5ad9315b951a92ab331b1b3c667481a638ddc
Magomed <Magomed.nagoev@yandex.ru>
2021-07-28 10:58:26 +0300

    first commit

---
+++ UberCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UberCloneApp
+//
+//  Created by Нагоев Магомед on 28.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
